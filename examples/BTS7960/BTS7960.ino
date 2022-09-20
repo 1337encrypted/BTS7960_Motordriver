9955d7fba2b2e9b3fb9ae2a66b740d16e54f38d2
Yash Herekar <46808309+1337encrypted@users.noreply.github.com>
2022-09-20 13:45:11 +0530

    Update BTS7960.ino

--- examples/BTS7960/BTS7960.ino
+++ examples/BTS7960/BTS7960.ino
@@ -1,13 +1,5 @@
 #include <BTS7960.h>
 
-/* The default constructor has pins assigned as:
- * L_EN = 2 
- * R_EN = 4
- * L_PWM = 5
- * R_PWM = 6
- * so no need of passing the paramaters through the parameter list
- */
-  
 #define L_EN 2
 #define R_EN 4
 #define L_PWM 5                             //pin 5 supports 980hz pwm frequency
@@ -15,36 +7,31 @@
 //#define L_IS 7
 //#define R_IS 8
 
-//BTS7960 default constructor
-//BTS7960 motor1()
-
 //Parametrized constructor
-BTS7960 motor1(L_EN, R_EN, L_PWM, R_PWM);
+BTS7960 motor1(L_EN, R_EN, L_PWM, R_PWM);   //This method will create a object of the class BTS7960
 
 void setup() {
-  Serial.begin(9600);
-  motor1.begin();
-  motor1.enable(); 
+  Serial.begin(9600);                       //begin the serial monitor for output
+  motor1.begin();                           //This method will set the motor driver pins as output
+  motor1.enable();                          //This method will set the L_EN and R_EN to HIGH or digitalWrite them to +5v/3v depending on your mcu
 }
 
 void loop() {
   
   for(int i=0; i<=255; i=i+10)
-  { 
-    //clockwise rotation
-    motor1.pwm = i;
-    motor1.front();
+  {
+    motor1.pwm = i;                         //Set the speed, by default the speed is set to 255 you can change it 
+    motor1.front();                         //front functions should turn the motor in clockwise direction
     delay(500);
   }
-  motor1.stop();
+  motor1.stop();                            //stop function should halt the motor by applying 0 pwm to both R_PWM and L_PWM
   delay(500);
   for(int i=0; i<=255; i=i+10)
   { 
-    //anti-clockwise rotation
-    motor1.pwm = i;
-    motor1.back();
+    motor1.pwm = i;                         //Set the speed
+    motor1.back();                          //back functions should turn the motor in anti-clockwise direction
     delay(500);
   }
   delay(500);
-  motor1.disable();
+  motor1.disable();                         //This method will set the L_EN and R_EN to LOW or digitalWrite them to 0v
 }
