a790231279dfd4a7eaba80f5b10227087e410f13
1337encrypted <>
2023-10-16 11:58:53 +0530

    renamed to src

--- cp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-cp '/tmp/BTS7960_Motordriver.h' '/Users/shady/Documents/Brushed motor control/BTS7960/src/BTS7960_Motordriver.h'
-cp '/tmp/BTS7960_Motordriver.cpp' '/Users/shady/Documents/Brushed motor control/BTS7960/src/BTS7960_Motordriver.cpp'
