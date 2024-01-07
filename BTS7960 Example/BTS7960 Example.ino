e2cd0ae5413e595bbade8d1e7aadc2c3aa40ad0a
Yash Herekar <smallaszero@gmail.com>
2024-01-07 13:35:37 +0530

    Update BTS7960 Example.ino

--- BTS7960 Example/BTS7960 Example.ino
+++ BTS7960 Example/BTS7960 Example.ino
@@ -1,11 +1,21 @@
-#include <BTS7960.h>
-//BTS7960 works between +3v and +5v so you can use a uno, mega or a nano.
-#define L_EN 2
-#define R_EN 4
-#define L_PWM 5                             //pin 5 supports 980hz pwm frequency
-#define R_PWM 6                             //pin 6 supports 980hz pwm frequency
+#include <BTS7960_Motordriver.h>
 
-BTS7960 motor1(L_EN, R_EN, L_PWM, R_PWM);   //This method will create a object of the class BTS7960
+//Motor PWM properties
+const uint32_t freq = 10000;
+const uint8_t resolution = 8;
+
+//BTS7960 motor driver 2 pin definitions
+constexpr uint8_t leftMotorsId = 1;
+constexpr uint8_t RPWM1 = 26;
+constexpr uint8_t LPWM1 = 27;                   
+constexpr uint8_t R_EN1 = 25;  
+constexpr uint8_t L_EN1 = 14;
+constexpr uint8_t R_IS1 = -1;        //Alarm pin
+constexpr uint8_t L_IS1 = -1;        //Alarm pin
+
+constexpr MOTORDEBUG=true;
+
+BTS7960_ESP32 motor1(L_EN1, R_EN1, LPWM1, RPWM1, freq, resolution, L_IS1, R_IS1, leftMotorsId, MOTORDEBUG);       //Create an object of class motor1
 
 void setup() {
   Serial.begin(9600);                       //begin the serial monitor for output
@@ -17,7 +27,7 @@ void loop() {
   
   for(int i=0; i<=255; i=i+10)
   {
-    motor1.pwm = i;                         //Set the speed, by default the speed is set to 255 you can change it 
+    motor1.speed = i;                         //Set the speed, by default the speed is set to 255 you can change it 
     motor1.front();                         //front functions should turn the motor in clockwise direction
     delay(500);
   }
@@ -25,10 +35,9 @@ void loop() {
   delay(500);
   for(int i=0; i<=255; i=i+10)
   { 
-    motor1.pwm = i;                         //Set the speed
+    motor1.speed = i;                         //Set the speed
     motor1.back();                          //back functions should turn the motor in anti-clockwise direction
     delay(500);
   }
   delay(500);
-  motor1.disable();                         //This method will set the L_EN and R_EN to LOW or digitalWrite them to 0v
 }
